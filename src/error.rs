76f50d914bd92be277e72b3dc39f1554b343381f
alickrxu <alickrxu@gmail.com>
2022-02-18 20:30:34 -0500

    create error.rs

---
+++ src/error.rs
@@ -0,0 +1,18 @@
+// Using this library so we don't need to implement fmt::Display for our new Error types 
+// (https://doc.rust-lang.org/rust-by-example/error/multiple_error_types/define_error_type.html)
+use thiserror::Error; 
+
+use solana_program::program_error::ProgramError;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+	// Invalid Instruction
+	#[error("Invalid Instruction")]
+	InvalidInstruction,
+}
+
+impl From<EscrowError> for ProgramError {
+	fn from(e: EscrowError) -> Self {
+		ProgramError::Custom(e as u32)
+	}
+}
